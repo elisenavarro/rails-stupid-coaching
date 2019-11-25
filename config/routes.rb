0e8843f6415767c0eadac7db64e1e7ab05724288
Elise Navarro <elise.marie.navarro@gmail.com>
2019-11-25 13:11:43 +0100

    Complete Stupid Coaching challenge with SCSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# config/routes.rb
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # verb 'path', to: 'controller#action', as: :route_name
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
